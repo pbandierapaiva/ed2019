84d0cceb8926f4dfb918b739cd927f88c83fbc8c
pbandierapaiva <paiva@unifesp.br>
2019-10-15 12:05:37 -0300

    Aula de 15/10

--- Alinhamento/alinha.c
+++ Alinhamento/alinha.c
@@ -5,20 +5,77 @@
 #include <string.h>
 #include <stdlib.h>
 
-int *alinhaSW(char *, char *, int, int, int);
+int *geraMatrizSW(char *, char *, int, int, int);
 void imprime(char *, char *, int *);
+int *maxValPtr(int *, int, int);
 
 int main() {
 	int *matriz;
-	char s1[] = "TGCA";
-	char s2[] = "TGAA";
+	int s1len, s2len;
+	int nrow, ncol;
+
+//	char s1[] = "TGCACT";
+//	char s2[] = "TGAG";
 	
-	matriz = alinhaSW(s1,s2, 5, -3, -4);
-	imprime(s1,s2,matriz);
+	char s1[] = "TGCACAGACTACGGAGCATCGACGGACTAGCAGCACTAGCT";
+	char s2[] = "TGAAACTACGGAGCATCGACGGACTAGCTGAGGCATTCAGCAG";
+//	char s1[] = "MNGTEGPDFYVPFSNATGVVRSPFEYPQYYLAEPWQFSMLAAYMFLLIVLGFPINFLTLYVTVQHKKLRTPLNYILLNLAVADLFMVFGGFTTTLYTSLHGYFVFGPTGCNIEGFFATLGGEIALWSLVVLAIERYVVVCKPMSNFRFGENHAIMGVAFTWVMALACAAPPLVGWSRYIPEGMQCSCGVDYYTLKPEVNNESFVIYMFVVHFTIPLIVIFFCYGQLVFTVKEAAAQQQESATTQKAEKEVTRMVILMVVFFLICWVPYASVAFYIFTHQGSNFGPIFMTLPAFFAKSSSIYNPVIYIMMNKQFRNCMLTTLCCGKNPLGDDEASATASKTETSQVAPA";
+//	char s2[] = "MNGTEGLNFYVPFSNKTGVVRSPFEYPQYYLAEPWQFSVLAAYMFLLIVLGFPINFLTLYVTVQHKKLRTPLNYILLNLAVANLFMVFGGFTTTLYTSLHAYFIFGPTGCNLEGFFATLGGEIALWSLVVLAIERYVVVCKPMSNFRFGENHAIMGLALTWIMAMACAAPPLVGWSRYIPEGMQCSCGIDYYTLSPEVNNESFVIYMFVVHFTIPLVIIFFCYGQLVFTVKEAAAQQQESATTQKAEKEVTRMVIIMVVAFLICWVPYASVAFYIFTHQGSDFGPIFMTIPSFFAKSSSIYNPVIYIMMNKQFRNCMLTTLCCGRNPLGDDEASTTASKTETSQVAPA";
+	s1len = strlen(s1);
+	s2len = strlen(s2);
+	nrow = s1len+1;
+	ncol = s2len+1;
+	matriz = geraMatrizSW(s1,s2, 5, -3, -4);
+
+
+//	imprime(s1,s2,matriz);
+	
 	return 0;
 }
 
-int *alinhaSW(char *s1, char *s2, int match, int mismatch, int gap) {
+int *traceback(int *S, int nlin, int ncol){
+		//char aln1[nlin+ncol], aln2[nlin+ncol];
+		int vetorDesloc[nlin+ncol];
+		int *p;
+		int i=0;
+		int *ps,*pe,*pd, *max;
+		
+		p = maxValPtr( S, nlin, ncol);
+		while( *p ) {
+			vetorDesloc[i]=p-S;
+			
+			ps = (p-ncol); //superior
+			pe = (p-1); // esquerda
+			pd = (p-ncol-1); // diagonal
+			if(pd<S) break;
+			if( *ps > *pe)
+			 	max=ps;
+			else
+				max=pe;
+			if(*pd >= *max)
+				max=pd;
+			p=max;
+						
+		} 
+		
+		return vetorDesloc;
+} 
+
+int *maxValPtr(int *S, int nlin, int ncol){
+ 	int *p, *maxp;
+	int maxVal=0;
+ 
+ 	maxVal = *S;
+ 	for(p=S, maxp=S; p-S < nlin*ncol; p++ ) {
+	 	if( *p>=maxVal ) {
+			maxVal=*p;
+			maxp=p;
+			}
+		}
+	return maxp;
+}
+
+int *geraMatrizSW(char *s1, char *s2, int match, int mismatch, int gap) {
 	int *S;
 	int w,h;
 	int max,cur;
